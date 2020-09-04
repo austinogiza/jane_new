7fbd2be968a56c06805506b4d2babbc3c2847d60
Augustine Ogiza <austinogiza@gmail.com>
2020-09-04 15:50:09 +0100

    jane

--- core/urls.py
+++ core/urls.py
@@ -2,10 +2,18 @@ from django.urls import path, include
 from . import views
 
 app_name ="jane"
+
+
 urlpatterns = [
 
     path('', views.HomeView.as_view(), name="home" ),
-        path('', views.ShopView.as_view(), name="shop" )
+    path('shop/', views.ShopView.as_view(), name="shop" ),
+    path('about-us/', views.AboutView.as_view(), name="about" ),
+    path('detail/', views.DetailView.as_view(), name="detail" ),
+    path('cart/', views.CartView.as_view(), name="cart" ),
+    path('checkout/', views.CheckoutView.as_view(), name="checkout" ),
+    
     
 
 ]
+
--- core/views.py
+++ core/views.py
@@ -9,3 +9,23 @@ class HomeView(TemplateView):
 
 class ShopView(TemplateView):
     template_name = 'shop-sidebar.html'
+
+class AboutView(TemplateView):
+    template_name = 'about.html'
+
+
+
+class DetailView(TemplateView):
+    template_name = 'product-single.html'
+
+
+class CheckoutView(TemplateView):
+    template_name = 'checkout.html'
+
+
+
+class CartView(TemplateView):
+    template_name = 'cart.html'
+
+def handler404(request, exception):
+    return render(request, '404.html')
