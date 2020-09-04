ec6eff85188283ba3473d50761e6098a73361d9f
Augustine Ogiza <austinogiza@gmail.com>
2020-09-04 13:57:39 +0100

    first commit

---
+++ core/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+# Create your views here.
+
+class HomeView(TemplateView):
+    template_name = 'index.html'
+
+
+class ShopView(TemplateView):
+    template_name = 'shop-sidebar.html'
