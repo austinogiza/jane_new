c1282ed595276bba1a8b67f2812a23dc36ca40bf
Augustine Ogiza <austinogiza@gmail.com>
2020-09-06 03:19:05 +0100

    jane

--- core/urls.py
+++ core/urls.py
@@ -12,6 +12,42 @@ urlpatterns = [
     path('detail/', views.DetailView.as_view(), name="detail" ),
     path('cart/', views.CartView.as_view(), name="cart" ),
     path('checkout/', views.CheckoutView.as_view(), name="checkout" ),
+    path('faq/', views.FaqView.as_view(), name="faq" ),
+       path('contact/', views.ContactView.as_view(), name="contact" ),
+        path('login/', views.LoginView.as_view(), name="login" ),
+        path('signup/', views.SignupView.as_view(), name="signup" ),
+            path('forgot-password/', views.ForgotView.as_view(), name="forgot" ),
+         path('blog/', views.BlogView.as_view(), name="blog" ),
+       path('blog/detail/', views.BlogDetailView.as_view(), name="blog-detail" ),
+              path('payment/success/', views.ConfirmView.as_view(), name="pay" ),
+                 path('payment/receipt/', views.PaystackView.as_view(), name="receipt" ),
+                 path('dashboard/', views.DashboardView.as_view(), name="dashboard" ),
+       path('dashboard/orders/', views.OrderView.as_view(), name="order" ),
+
+      path('dashboard/address/', views.AddressView.as_view(), name="address" ),
+    #    path('pricing/', views.PricingView.as_view(), name="pricing" ),
+
+
+
+       
+
+       
+                 
+
+
+       
+        
+
+
+
+
+
+        
+
+       
+    
+
+    
     
     
 
--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+from django.contrib import messages
 # Create your views here.
 
 class HomeView(TemplateView):
@@ -26,6 +27,64 @@ class CheckoutView(TemplateView):
 
 class CartView(TemplateView):
     template_name = 'cart.html'
+
+
+class FaqView(TemplateView):
+    template_name = 'faq.html'
+
+
+
+class ContactView(TemplateView):
+    template_name = 'contact.html'
+
+
+class LoginView(TemplateView):
+    template_name = 'login.html'
+
+
+class SignupView(TemplateView):
+    template_name = 'signin.html'
+
+
+
+class ForgotView(TemplateView):
+    template_name = 'forget-password.html'
+
+
+class BlogView(TemplateView):
+    template_name = 'blog-grid.html'
+
+
+class BlogDetailView(TemplateView):
+    template_name = 'blog-single.html'
+
+
+class ConfirmView(TemplateView):
+    template_name = 'confirmation.html'
+
+
+
+class PaystackView(TemplateView):
+    template_name =   "purchase-confirmation.html"
+
+class OrderView(TemplateView):
+    template_name =   "order.html"
+
+class DashboardView(TemplateView):
+    template_name =   "dashboard.html"
+
+def click(request):
+    messa
+    return render(request, 'contact.html')
+
+
+# class PricingView(TemplateView):
+#     template_name =   "pricing.html"
+
+class AddressView(TemplateView):
+    template_name =   "address.html"
+
+
 
 def handler404(request, exception):
     return render(request, '404.html')
